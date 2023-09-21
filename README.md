8b004f26bbdccae2859056fc736347ffeafd6129
f3ve <free.eve@pm.me>
2023-09-21 12:25:29 -0800

    Add licenses;

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Freedomevenden.com
 
 My personal website
+
+Code is licensed under [MIT](./LICENSE).
+
+Text and images are licensed under [CC-BY-NC-SA-4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/)
--- src/core/components/CoreFooter.vue
+++ src/core/components/CoreFooter.vue
@@ -3,7 +3,27 @@ import dayjs from 'dayjs';
 </script>
 
 <template>
-  <footer class="flex items-center px-4 text-sm">
-    <p>&#169; {{ dayjs().format('YYYY') }} Freedom Evenden</p>
+  <footer class="flex items-center px-4 text-sm prose w-full max-w-full">
+    <p class="op-50">
+      &#169; {{ dayjs().format('YYYY') }}
+      <a
+        xmlns:cc="http://creativecommons.org/ns#"
+        href="freedomevenden.com"
+        property="cc:attributionName"
+        rel="cc:attributionURL"
+        >{{ ' ' }}
+        Freedom Evenden
+        {{ ' ' }}
+      </a>
+      -
+      <span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">
+        Website text and images
+      </span>
+      are licensed under
+      <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
+        CC-BY-NC-SA-4.0
+      </a>
+      .
+    </p>
   </footer>
 </template>
