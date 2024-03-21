94a2f9ab5f883bd670cb33ebcc52b680733ac073
f3ve <free.eve@pm.me>
2024-03-20 19:47:06 -0800

    created the carousel component;

--- src/components/projects/ProjectCarousel.vue
+++ src/components/projects/ProjectCarousel.vue
@@ -1,18 +1,45 @@
 <script lang="ts" setup>
-import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+import { Carousel, Slide, Navigation } from 'vue3-carousel';
+
+const props = defineProps<{
+  images?: string[];
+  videos?: { webm: string; mp4: string }[];
+}>();
+
+const imagesComp = computed(() => (props.images ? props.images : []));
+const videosComp = computed(() => (props.videos ? props.videos : []));
+const currentSlide = ref(0);
+
+function next() {
+  currentSlide.value++;
+}
+
+function prev() {
+  currentSlide.value--;
+}
 </script>
 
 <template>
-  <Carousel \>
-    <Slide :key="1">
-      <img
-        src="https://lh3.googleusercontent.com/pw/AP1GczPL-koznilK5pEBP1iWEvYsjdsBqNBo2A41C50xABPtGSXPpdjY2Aj_PcaVmzHo4zdYTJ4GM4ZIIwwV3B6BCp0LdhzcLapvE9oeUK0uJtMA7tYmGceasXRz91xfCdJ-ldfhih-7LrelVjvEJfsyeXhA=w1279-h719-s-k-no-gm?authuser=0"
-      />
-    </Slide>
-    <Slide :key="2">Another slide yay!</Slide>
-
-    <template #addons>
-      <Navigation />
-    </template>
-  </Carousel>
+  <div class="relative">
+    <Carousel v-model="currentSlide" wrap-around :items-to-show="1.1">
+      <Slide v-for="image in imagesComp" :key="image">
+        <div class="h-full w-full px-1 shadow">
+          <img :src="image" class="object-contain transition-transform" />
+        </div>
+      </Slide>
+      <Slide v-for="video in videosComp" :key="video.webm">
+        <video>
+          <source :src="video.webm" type="video/webm" />
+          <source :src="video.mp4" type="video/mp4" />
+          <p>Your browser doesn't support this video format.</p>
+        </video>
+      </Slide>
+    </Carousel>
+    <div
+      class="pointer-events-none absolute top-0 h-full w-full flex justify-between"
+    >
+      <CoreBtn no-bg class="pointer-events-initial" @click="prev">Prev</CoreBtn>
+      <CoreBtn no-bg class="pointer-events-initial" @click="next">Next</CoreBtn>
+    </div>
+  </div>
 </template>
